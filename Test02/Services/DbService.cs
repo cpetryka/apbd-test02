6b9b1f0fdeecb7c0f6bebefb8ad5ab9fe8528db8
Cezary Petryka <cezaryp10@gmail.com>
2024-06-11 14:52:50 +0200

    Add database service

---
+++ Test02/Services/DbService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test02.Data;
+using Test02.DTOs;
+using Test02.Models;
+
+namespace Test02.Services;
+
+public class DbService : IDbService
+{
+    private readonly DatabaseContext _context;
+    
+    public DbService(DatabaseContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<ICollection<Character>> GetCharacterData(int idx)
+    {
+        return await _context.Characters
+            .Include(c => c.Backpacks)
+                .ThenInclude(b => b.Item)
+            .Include(c => c.CharacterTitles)
+                .ThenInclude(ct => ct.Title)
+            .Where(c => c.Id == idx)
+            .ToListAsync();
+    }
+}
---
+++ Test02/Services/IDbService.cs
@@ -0,0 +1,9 @@
+﻿using Test02.DTOs;
+using Test02.Models;
+
+namespace Test02.Services;
+
+public interface IDbService
+{
+    Task<ICollection<Character>> GetCharacterData(int idx);
+}
