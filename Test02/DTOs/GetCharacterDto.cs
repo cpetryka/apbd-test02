c61cf4ed03e7b1b03b13487e99774015f7793d20
Cezary Petryka <cezaryp10@gmail.com>
2024-06-11 14:52:27 +0200

    Add GET DTOs

---
+++ Test02/DTOs/GetCharacterDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Test02.DTOs;
+
+public class GetCharacterDto
+{
+    public string FirstName { get; set; }
+    public string FLastName { get; set; }
+    public int CurrentWeight { get; set; }
+    public int MaxWeight { get; set; }
+    public ICollection<GetBackpackItemDto> BackpackItems { get; set; } = null!;
+    public ICollection<GetTitleDto> Titles { get; set; } = null!;
+}
+
+public class GetBackpackItemDto {
+    public string ItemName { get; set; }
+    public int ItemWeight { get; set; }
+    public int Amount { get; set; }
+}
+
+public class GetTitleDto
+{
+    public string Title { get; set; }
+    public DateTime AcquiredAt { get; set; }
+}
