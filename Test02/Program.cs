a73e856cc95e4c632e4800f9753888bb4177296d
Cezary Petryka <cezaryp10@gmail.com>
2024-06-11 14:25:31 +0200

    Register database context

--- Test02/Program.cs
+++ Test02/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Test02.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>(
+    options => options.UseSqlServer("Name=ConnectionStrings:Default"));
 
 var app = builder.Build();
 
